f6b38f1b1c3827529fd13194c1e1cb19aa71ac3f
sevketopuz <svktpz@gmail.com>
2015-10-17 18:41:47 +0300

    finding index of tweet hastags

---
+++ text.py
@@ -0,0 +1,19 @@
+import json
+
+tweets_data_path = 'tweetdata.txt'
+
+tweets_data = []
+tweets_file = open(tweets_data_path, "r")
+for line in tweets_file:
+    try:
+        tweet = json.loads(line)
+        tweets_data.append(tweet)
+    except:
+        continue
+
+#print len(tweets_data)
+tw = tweets_data[0]
+tw_text = tw['text']
+print [pos for pos, char in enumerate(tw_text) if char == '#']
+#for tw in tweets_data:
+#	print tw['text']
