9e85650ee3f6bef5295f71cc8103e79093f0f6e5
sevketopuz <svktpz@gmail.com>
2015-11-10 01:41:30 +0200

    Database operations has been added.

--- text.py
+++ text.py
@@ -1,5 +1,4 @@
 import json
-import unicodedata
 
 tweets_data_path = '../trash/dataset1.json'
 
--- writeToDatabase.py
+++ writeToDatabase.py
@@ -1,4 +1,5 @@
 import sqlite3
+import json
 
 print "What do you want to do?"
 print "[1] INSERT DATASETS TO DATABASE"
@@ -6,10 +7,24 @@ print "[2] CREATE DATABASE"
 
 option = input(" ~ ")
 
-if option == 1:
+conn = sqlite3.connect('tweets.db')
 
-	conn = sqlite3.connect('tweets.db')
-	
+def insertToTexts(docId, body):
+
+	c = conn.cursor()
+	c.execute('insert into TEXTS values (?,?)', (docId, body))
+	conn.commit()
+	conn.close()
+
+def insertToHashtags(docId, hasht):
+
+	c = conn.cursor()
+	c.execute('insert into HASHTAGS values (?,?)', (docId, hasht))
+	conn.commit()
+	conn.close()
+
+if option == 2:
+
 	conn.execute('''CREATE TABLE TEXTS
 	       (ID INT PRIMARY KEY     NOT NULL,
 	       BODY        CHAR(200));''')
@@ -22,6 +37,12 @@ if option == 1:
 
 	conn.close()
 
-if option == 2:
+if option == 1:
 
-	conn = sqlite3.connect('tweets.db')
+	print "Which dataset do you want to insert?"
+	
+	setNum = raw_input(" ~ ")
+	s = 'dataset' + setNum + '.json'
+	tweets_data_path = s
+	
+	#insertToTexts(123,'ahmet')
