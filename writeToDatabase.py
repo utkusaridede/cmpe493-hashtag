5e83985ad2c46183bdb039c6a4d677466ac7a682
utkusaridede <utkusaridede@gmail.com>
2015-11-09 05:51:26 +0200

    Report, database file and database operations have been added.

---
+++ writeToDatabase.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+print "What do you want to do?"
+print "[1] INSERT DATASETS TO DATABASE"
+print "[2] CREATE DATABASE"
+
+option = input(" ~ ")
+
+if option == 1:
+
+	conn = sqlite3.connect('tweets.db')
+	
+	conn.execute('''CREATE TABLE TEXTS
+	       (ID INT PRIMARY KEY     NOT NULL,
+	       BODY        CHAR(200));''')
+
+	conn.execute('''CREATE TABLE HASHTAGS
+	       (ID INT PRIMARY KEY     NOT NULL,
+	       HASHTAG     CHAR(100));''')
+
+	print "Table created successfully";
+
+	conn.close()
+
+if option == 2:
+
+	conn = sqlite3.connect('tweets.db')
