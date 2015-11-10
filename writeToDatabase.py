1413eb5dc9cb981a75bc26f4e8a5c827700b11d6
sevketopuz <svktpz@gmail.com>
2015-11-10 02:10:29 +0200

    The last form of the project before midterm report successfully finished.

--- writeToDatabase.py
+++ writeToDatabase.py
@@ -14,14 +14,12 @@ def insertToTexts(docId, body):
 	c = conn.cursor()
 	c.execute('insert into TEXTS values (?,?)', (docId, body))
 	conn.commit()
-	conn.close()
 
 def insertToHashtags(docId, hasht):
 
 	c = conn.cursor()
 	c.execute('insert into HASHTAGS values (?,?)', (docId, hasht))
 	conn.commit()
-	conn.close()
 
 if option == 2:
 
@@ -30,7 +28,7 @@ if option == 2:
 	       BODY        CHAR(200));''')
 
 	conn.execute('''CREATE TABLE HASHTAGS
-	       (ID INT PRIMARY KEY     NOT NULL,
+	       (ID INT     NOT NULL,
 	       HASHTAG     CHAR(100));''')
 
 	print "Table created successfully";
@@ -44,5 +42,51 @@ if option == 1:
 	setNum = raw_input(" ~ ")
 	s = 'dataset' + setNum + '.json'
 	tweets_data_path = s
+
+	tweets_data = []
+	tweets_file = open(tweets_data_path, "r")
 	
-	#insertToTexts(123,'ahmet')
+	for line in tweets_file:
+		try:
+			tweet = json.loads(line)
+			tweets_data.append(tweet)
+		except:
+			continue
+
+	for tw in tweets_data:
+		tw_text = tw['text']
+		tw_text = tw_text.replace('\n', ' ')
+		tw_text = tw_text + ' '
+		tw_id = tw['id']
+
+		insertToTexts(tw_id,tw_text)
+
+		hash_index = [pos for pos, char in enumerate(tw_text) if char == '#']
+		space_index = [pos for pos, char in enumerate(tw_text) if char == ' ']
+
+		spaceCounter = 0
+		for i in range(0,len(hash_index)):
+			for k in range(0,len(space_index)):
+
+				if i < len(hash_index)-1:
+					if hash_index[i+1] > space_index[spaceCounter] and hash_index[i] > space_index[spaceCounter]:
+						spaceCounter += 1
+					elif hash_index[i+1] > space_index[spaceCounter] and hash_index[i] < space_index[spaceCounter]:
+						hashtag = tw_text[hash_index[i]+1:space_index[spaceCounter]]
+						if len(hashtag) != 0:
+							insertToHashtags(tw_id,hashtag)
+						break
+					else:
+						hashtag = tw_text[hash_index[i]+1:hash_index[i+1]]
+						if len(hashtag) != 0:
+							insertToHashtags(tw_id,hashtag)
+						break
+				else:
+					if hash_index[i] > space_index[spaceCounter]:
+						spaceCounter += 1
+					else:
+						hashtag = tw_text[hash_index[i]+1:space_index[spaceCounter]]
+						if len(hashtag) != 0:
+							insertToHashtags(tw_id,hashtag)
+						break
+conn.close()
