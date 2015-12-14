1d8eb95a971c54a532f0d2cdfec5afe0b39b32d2
utkusaridede <utkusaridede@gmail.com>
2015-12-15 00:44:15 +0200

    Hashtag verfication is extended.

--- writeToDatabase.py
+++ writeToDatabase.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
 import sqlite3
 import json
 
@@ -12,13 +14,13 @@ conn = sqlite3.connect('tweets.db')
 def insertToTexts(docId, body):
 
 	c = conn.cursor()
-	c.execute('insert into TEXTS values (?,?)', (docId, body))
+	c.execute('insert or ignore into TEXTS values (?,?)', (docId, body))
 	conn.commit()
 
 def insertToHashtags(docId, hasht):
 
 	c = conn.cursor()
-	c.execute('insert into HASHTAGS values (?,?)', (docId, hasht))
+	c.execute('insert or ignore into HASHTAGS values (?,?)', (docId, hasht))
 	conn.commit()
 
 if option == 2:
@@ -29,7 +31,7 @@ if option == 2:
 
 	conn.execute('''CREATE TABLE HASHTAGS
 	       (ID INT     NOT NULL,
-	       HASHTAG     CHAR(100));''')
+	       HASHTAG CHAR(100) UNIQUE);''')
 
 	print "Table created successfully";
 
@@ -56,13 +58,43 @@ if option == 1:
 	for tw in tweets_data:
 		if not 'limit' in tw:
 			tw_text = tw['text']
-			if not tw_text.startswith('RT'):
-				tw_text = tw_text.replace('\n', ' ')
-				tw_text = tw_text + ' '
-				tw_id = tw['id']
+			#if not tw_text.startswith('RT'):
+			tw_text = tw_text.replace('\n', ' ')
+			tw_text = tw_text + ' '
+			tw_id = tw['id']
 
-				insertToTexts(tw_id,tw_text)
+			insertToTexts(tw_id,tw_text)
 
+			space_index = [pos for pos, char in enumerate(tw_text) if char == ' ']
+
+			counter = 0
+			for k in range(0,len(space_index)):
+
+				string = tw_text[counter:space_index[k]]
+				if string.startswith('#'):
+					string = string[1:]
+
+					if len(string) > 0 and not '#' in string:
+						if string.startswith('_') and len(string) == 1:
+							counter = space_index[k] + 1
+						else:
+							index = 0
+							for s in string:
+								if not s in u"_abcçdefgğhıijklmnoöprsştuüvyzABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ0123456789":
+									break
+								else:
+									index = index + 1
+							hashtag = string[0:index]
+							if len(hashtag) > 0:
+								insertToHashtags(tw_id,hashtag)
+							counter = space_index[k] + 1
+					else:
+						counter = space_index[k] + 1
+				else:
+					counter = space_index[k] + 1
+conn.close()
+
+'''
 				hash_index = [pos for pos, char in enumerate(tw_text) if char == '#']
 				space_index = [pos for pos, char in enumerate(tw_text) if char == ' ']
 
@@ -91,4 +123,4 @@ if option == 1:
 								if len(hashtag) != 0:
 									insertToHashtags(tw_id,hashtag)
 								break
-conn.close()
+'''
