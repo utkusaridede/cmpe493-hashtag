95edd061e49918c42d9904730a10b328b0ad8bb9
utkusaridede <utkusaridede@gmail.com>
2016-01-10 20:42:47 +0200

    Segmentation process debug.

--- testData/segment_hashtags.py
+++ testData/segment_hashtags.py
@@ -16,9 +16,11 @@ indexSeg = 0
 for line in hastagLines:
 	line = line.replace("\n", "")
 	string = line
+	space = 0
 	for i in range(0, len(line)):
 		if "B" in BIlines[i+index] and i != 0:
-			string = string[:i] + " " + string[i:]
+			string = string[:i+space] + " " + string[i+space:]
+			space += 1
 	index += len(line)
 	lineSeg = segmentLines[indexSeg].replace("\n", "")
 	output.write(lineSeg)
